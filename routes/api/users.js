9fd3a4dc706f4e834cacb722b010fa968aa99422
felipeas314 <felipeas314@gmail.com>
2019-09-02 21:19:17 -0300

    criaçao da rota de usuario

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-router.get('/', (req, res) => res.send('Usr route'));
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ errors: 'User already exists' });
+      }
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.status(201).json('ok');
+    } catch (err) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
-
+app.use(express.json());
 connectDB();
 
 app.get('/', (req, res) => {
