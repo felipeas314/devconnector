8de2f5d016b60d224b048398fc578a2164c8ac0d
felipeas314 <felipeas314@gmail.com>
2019-09-03 20:38:18 -0300

    implementaçao do middleware de segurança

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
+const auth = require('../../middleware/au');
 
 router.get('/', (req, res) => res.send('Usr route'));
 
