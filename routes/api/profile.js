4c904b46b62d914140442a66ceeebe651a3581c1
felipeas314 <felipeas314@gmail.com>
2019-09-03 20:45:17 -0300

    implementaçao para mostrar o seu profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,8 +2,28 @@ const express = require('express');
 const request = require('request');
 const config = require('config');
 const router = express.Router();
-const auth = require('../../middleware/au');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
 
-router.get('/', (req, res) => res.send('Usr route'));
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.status(200).json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 module.exports = router;
